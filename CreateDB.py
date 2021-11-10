dbfab3b534bf7d06c661acb394b917946bfbd61c
MattBenyon <83309705+MattBenyon@users.noreply.github.com>
2021-11-10 13:48:18 +0000

    Added files correct as of 10/11/11 at 13:48

---
+++ CreateDB.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  1 13:06:25 2021
+
+@author: mattb
+"""
+
+from psycopg2 import connect
+
+
+def createDB(db_name, db_user,db_password):
+    try:
+
+        conn = connect(
+            dbname=db_name,
+          user=db_user,
+          host="localhost",
+          password=db_password)
+
+        cursor = conn.cursor()
+
+        with open("DataVaultV4.txt") as f:
+          vault_script = f.read()
+
+        print("Creating database...")
+        cursor.execute(vault_script)
+
+        conn.commit()
+
+        cursor.close()
+
+        conn.close()
+
+    except:
+        print("\nThe credentials were incorrect or the user does not have correct permissions. Please correct this issue and run the program again.")
+        quit()
