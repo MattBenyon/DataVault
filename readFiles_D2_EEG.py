fc09bcec822ae281dcf6e3f9c587c17896f26672
MattBenyon <83309705+MattBenyon@users.noreply.github.com>
2021-12-02 20:09:23 +0000

    numpy

--- readFiles_D2_EEG.py
+++ readFiles_D2_EEG.py
@@ -1,23 +1,23 @@
 import mat73
-import pandas as pd
+
 
 def eegData(filepath):
 
     data_dict = mat73.loadmat(filepath)
-    eegData = pd.DataFrame(data_dict['Data'])
+    eegData = data_dict['Data']
 
     return eegData
 
 def joinData(filepath1, filepath2):
 
     data_1 = mat73.loadmat(filepath1)
-    data1 = pd.DataFrame(data_1['Data'])
+    data1 = data_1['Data']
 
     data_2 = mat73.loadmat(filepath2)
-    data2 = pd.DataFrame(data_2['Data'])
+    data2 = data_2['Data']
 
     data = [data1, data2]
 
-    joinedData = pd.concat(data, axis=1)
+    joinedData = np.concatenate(data, axis=1)
 
     return joinedData
